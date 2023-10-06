af85c7a814a39b493a85fdc8b630115cad2c12da
Mustafa Fatih GUCLUER <fatihgucluer@hotmail.com>
2023-10-06 11:18:42 +0300

    Initial Commit

---
+++ YemekteNeVar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YemekteNeVar
+//
+//  Created by Mustafa Fatih on 6/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
