7732028cd6123246e924b05ab8ba969f5bc45bb6
Mustafa Fatih GUCLUER <fatihgucluer@hotmail.com>
2023-10-07 12:31:58 +0300

    Design and preview are done

--- YemekteNeVar/ViewController.swift
+++ YemekteNeVar/ViewController.swift
@@ -1,17 +1,25 @@
-//
-//  ViewController.swift
-//  YemekteNeVar
-//
-//  Created by Mustafa Fatih on 6/10/23.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var food1: UILabel!
+    @IBOutlet weak var food2: UILabel!
+    @IBOutlet weak var food3: UILabel!
+    @IBOutlet weak var food4: UILabel!
+    @IBOutlet weak var calorie: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        imageView.image = UIImage(named: "menu.jpeg")
+        dateLabel.text = "9/10/2023"
+        food1.text = "Ezogelin Corba 170 kkcal"
+        food2.text = "Kuru Fasulye 250kkcal"
+        food3.text = "Pirinc Pilavi 300 kkcal "
+        food4.text = "Trilece 400 kkcal"
+        calorie.text = "1120 kkcal"
     }
 
 
